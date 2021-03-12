38a48a0ed636e0966e8ccc563d93dc2a70fb73f5
Arthur <arthur.toebosch@student.hku.nl>
2021-03-12 14:58:06 +0100

    Commit V7.00

---
+++ Assets/EndGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public GameObject winScreen;
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        winScreen.SetActive(true);
+        Cursor.lockState = CursorLockMode.Confined;
+        Destroy(player);
+    }
+
+}
